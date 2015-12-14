ab6f35fd721b70fa0f9dac2464863a8a66fedb81
Dominique Wu <kuanyiwudesign@gmail.com>
2015-12-13 16:52:59 -0800

    basic layout for eye-catcher_01

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+function sideNav() {
+  if ($(window).width() < 769) {
+    $('.off-canvas-wrap').removeClass('move-right');
+    $('.left-off-canvas-toggle').show();
+  } else {
+    $('.off-canvas-wrap').addClass('move-right');
+    $('.left-off-canvas-toggle').hide();
+  }  
+}
+
+$(window).resize(function() {
+  // sideNav();
+});
+
