dd5e204b14f61f7c70d2e1a9c5c990eb2c3cb059
Russell Jahn <russelljahn@gmail.com>
2015-12-27 16:16:32 -0600

    Fixing uncommitted changes, added lazyload, velocity, and imagesloaded.

--- js/index.js
+++ js/index.js
@@ -2,6 +2,10 @@ $(document).ready(function () {
 
     var sk = skrollr.init();
 
+    $(".wrapper").imagesLoaded({ background: true }, function () {
+        console.log(".wrapper background image loaded");
+    });
+
     //Hack to make Skrollr parallax the whole document background
     window.dispatchEvent(new Event('resize'));
 
